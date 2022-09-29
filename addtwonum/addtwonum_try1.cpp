f193d5e0ebc80d1b599f117d732804dc3816d2a2
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-29 13:57:19 -0500

    Update addtwonum_try1.cpp
    Cleaned up the code to take less time, added comments

--- addtwonum/addtwonum_try1.cpp
+++ addtwonum/addtwonum_try1.cpp
@@ -11,24 +11,24 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0;
-        ListNode head;
-        ListNode *tmp = &head;
-        while(l1||l2)
+        int carry = 0; //Creating carry function
+        ListNode answer; //Declaring where our answer would be stored
+        ListNode *tmp = &answer; //Temp variable to work on
+        while(l1||l2) //While either l1 or l2 is active
         {
-            int val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
-            carry = val/10;
-            val = val%10;
-            ListNode *after = new ListNode(val);
-            tmp->next = after;
-            tmp = tmp->next;
-            (l1 ? l1 = l1->next:0);
-            (l2 ? l2 = l2->next:0);
+            int val = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0); //value = carry + if(l1) then l1->val else 0 + if(l2) then l2->val else 0
+            carry = val/10; //Carry the tens place
+            val = val%10; //Value is just the units place.
+            ListNode *after = new ListNode(val); //Declare the next number as the subsequent digit in the linked list
+            tmp->next = after; //Join the after variable to the answer linked list
+            tmp = tmp->next; //Iterate temp one step down
+            (l1 ? l1 = l1->next:0); //If l1 exists, initialize l1 one step down
+            (l2 ? l2 = l2->next:0); //If l2 exists, initialize l2 one step down
         }
-        if(carry)
+        if(carry) //If we have a final carry
         {
-            tmp->next = new ListNode(carry);
+            tmp->next = new ListNode(carry); //Since no more digits, the carry is the end/start number
         }
-        return head.next;
+        return answer.next; //Answer was the top of the list. The digits started from the "next"
     }
 };
