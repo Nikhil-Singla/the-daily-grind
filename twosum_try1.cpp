7b4d6892aad8b53955a17dbdbe616031e939b451
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-28 21:35:32 -0500

    Update twosum_try1.cpp
    Added comments to the code

--- twosum_try1.cpp
+++ twosum_try1.cpp
@@ -2,16 +2,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i = 0; i<nums.size()-1; i++)
+        for(int i = 0; i<nums.size()-1; i++) //First run through the entire array with the i'th element starting from 0 
         {
-            for(int j = i+1; j<nums.size();j++)
+            for(int j = i+1; j<nums.size();j++) //At the same time, run with the j'th element which is starting from i+1
             {
-                if((nums[i]+nums[j])==target)
+                if((nums[i]+nums[j])==target) //If the sum of i'th and the j'th element is target, then output
                 {
-                    return{i, j};
+                    return{i, j}; //Return as vector
                 }
             }
         }
-        return {};
+        return {}; //Return null if no solution
     }
 };
