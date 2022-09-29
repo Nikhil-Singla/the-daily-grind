eac4b77ff282b03696e45fe8c2b31ff1e9dac351
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-28 21:38:57 -0500

    Update twosum_try2.cpp
    Added comments to the solution.

--- twosum_try2.cpp
+++ twosum_try2.cpp
@@ -2,28 +2,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<pair<int, int>> pairs;
+        vector<pair<int, int>> pairs; //Declare vector of datatype "pair" and both elements of the pair/tuple being int
         for(int i=0; i<nums.size();i++)
         {
-            pairs.push_back({nums[i],i});
+            pairs.push_back({nums[i],i}); //Push the elements of the original vector to the pair vector
         }
-        sort(pairs.begin(), pairs.end());
-        int left = 0, right = nums.size()-1;
+        sort(pairs.begin(), pairs.end()); //Sort the pair's vector to arrange
+        int left = 0, right = nums.size()-1; //Assign two "markers". Left and Right
         while(left<right)
         {
-            if(pairs[left].first + pairs[right].first == target)
+            if(pairs[left].first + pairs[right].first == target) //If statement to match target. First refers to the first number of the pair.
             {
-                return {pairs[left].second, pairs[right].second};
+                return {pairs[left].second, pairs[right].second}; //Return the second number of the pair which is the index of the element from the original vector
             }
-            if(pairs[left].first + pairs[right].first < target)
+            if(pairs[left].first + pairs[right].first < target) //If the sum is below target, we need to increase min value by going right from the lowest (left) marker
             {
                 left++;
             }
-            if(pairs[left].first + pairs[right].first > target)
+            if(pairs[left].first + pairs[right].first > target) //If the sum is above target, we need to dec. max value by going left from the highest (right) marker
             {
                 right--;
             }
         }
-        return {};
+        return {}; //Return null if no solution
     }
 };
