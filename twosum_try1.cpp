2260edc830fba569baf13056e464d92f5c62c278
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-09-28 21:31:23 -0500

    Create twosum_try1.cpp
    First attempt at coding (Not proofread)

---
+++ twosum_try1.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int a=0, b=0;
+        vector<int> result;
+        for(int i = 0; i<nums.size()-1; i++)
+        {
+            for(int j = i+1; j<nums.size();j++)
+            {
+                if((nums[i]+nums[j])==target)
+                {
+                    return{i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
