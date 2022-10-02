cb95c7e13df266c91f047b82face347057da93e7
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-02 06:01:18 -0500

    Update sortedarray_try1.cpp
    Found the brute force solution. Was missing the condition for either of the nums being empty. After that, it all worked properly. Will refine when more knowledgable.

--- medianof2sortarr/sortedarray_try1.cpp
+++ medianof2sortarr/sortedarray_try1.cpp
@@ -2,6 +2,36 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
+        int a=0;
+        double b;
+        if(nums1.size()==0)
+        {
+            a = nums2.size();
+            if(a%2==0)
+            {
+                b=(double)nums2[(a/2)-1]+(double)nums2[(a/2)];
+                b /= 2;
+            }
+            else
+            {
+                b=(double)nums2[(a/2)];
+            }
+            return b;
+        }
+        if(nums2.size()==0)
+        {
+            a = nums1.size();
+            if(a%2==0)
+            {
+                b=(double)nums1[(a/2)-1]+(double)nums1[(a/2)];
+                b /= 2;
+            }
+            else
+            {
+                b=(double)nums1[(a/2)];
+            }
+            return b;
+        }
         auto start1=nums1.begin(),start2=nums2.begin();
         auto end1=nums1.end(),end2=nums2.end();
         int len=nums1.size()+nums2.size();
