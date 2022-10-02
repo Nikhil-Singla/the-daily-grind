dde92e4bc86e68212705646377ac837fcab1b615
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-02 01:48:32 -0500

    Update withoutrepeatingchar_try1.cpp
    Finally working solution

--- longestsubstring/withoutrepeatingchar_try1.cpp
+++ longestsubstring/withoutrepeatingchar_try1.cpp
@@ -1,86 +1,21 @@
+//After a whole bunch of mindtrickery, finally got around to getting a working solution with a lot of help.
+//Sliding Window Solution
 # include <string>
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int> hash(256, -1);
-        int length = s.length(), result=0, var=0;
-        if(length<2)
+    int lengthOfLongestSubstring(string str) {
+        unordered_map<char, int>map; //Creates a map of characters, where the key is a char and it points to a value.
+        int maxlen = 0; //Initializing the result
+        for(int point1 = 0, point2 = 0; point2 < str.size(); point2++) //Our second "pointer" moves down the array
         {
-            return length;
-        }
-        else if(length == 2)
-        {
-            if(s.at(0)==s.at(1))
-            {return(1);}
-            else
-            {return 2;}
-        }
-        auto first=s.begin(), second=s.end();
-        auto temp = first+1;
-        int ascii=0;
-        ascii = int(*first);
-        hash[ascii] = 1;
-        length = 1;
-        while(temp<=second)
-        {
-            while(*temp!=*first && temp<=second)
+            map[str[point2]]++; //As our pointer gets a character in the string, we increment the value of that character
+                                //in the map by 1
+            while(map[str[point2]] > 1) //If at any point the value of the character hits 2, it means we have reached a repeated character which we need to resolve by the sliding window.
             {
-                ascii = int(*temp);
-                if(hash[ascii]==1)
-                {
-                    if(result<var)
-                    {
-                        result = var;
-                    }
-                    first = temp++;
-                    if(*first==*temp)
-                    {var = 0;}
-                    else
-                    {var = 1;}
-                }
-                hash[ascii] = 1;
-                var++;
+                map[str[point1++]]--; //Now we move our window frame up, and decrement all the values we pass as they are removed from the frame.
             }
-            if(*first==*temp)
-            {var = 0;}
-            else
-            {var = 1;}
-            first = temp++;
+            maxlen = max(maxlen, point2 - point1 + 1); //We find if our current length is higher, or the old one. The +1 is because we need to count the initial starting point in the length (I think)
         }
-        return result;
+        return maxlen; //Return the max value
     }
 };
-
-//OLD Code for experience
-        /*int len = 0, size=s.size();
-        string::iterator first, second, last;
-        first = s.begin();
-        second = first+1;
-        last = s.end()-2;
-        int temp = 0;
-        int furst = -1, seconduh = -1;
-        while(second <= last)
-        {
-            temp = 1;
-            // std::cout<<*first;
-            vector<pair<int,int>> hash_set(256, -1);
-            furst = *first;
-            seconduh = *second;
-            hash_set[furst] = 1;
-            hash_set[seconduh] = 1;
-            while((hash_set[*second]!=1) && (second<=last))
-            {
-                hash_set[*second] = 1;
-                ++temp;
-                ++second;
-                std::cout<<*second;
-            }
-            ++second;
-            // std::cout<<*second;
-            first = second;
-            if(len<temp)
-            {
-                len = temp;
-            }
-        }
-        return len;*/
