7fbc79de0341098cd5bb454aad6fb9e7a4a8c3df
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-02 22:24:54 -0500

    Create substring_try1.cpp
    First try, incomplete code, ending for the day.

---
+++ longestpalindromic/substring_try1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if(s.length()==1)
+        {
+            return s;
+        }
+        auto first = s.begin(), last=s.end();
+        for(auto mid=s.begin();mid<last;mid++)
+        {
+            
+        }
+        return "a";
+    }
+};
