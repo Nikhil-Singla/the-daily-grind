2513eeee279a987215010f95379b058ef75f6a7c
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-04 05:40:18 -0500

    Update 1379_CorrespondingNodeinTree.cpp

--- easy_problems/1379_CorrespondingNodeinTree.cpp
+++ easy_problems/1379_CorrespondingNodeinTree.cpp
@@ -7,6 +7,35 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+//BETTER SOLUTION TO ACCOUNT FOR REPEATED VALUES
+class Solution {
+public:
+    TreeNode* result;
+    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
+        //Inorder = (Left Value Right)
+        //Preorder = (V L R)
+        //Postorder = (L R V)
+        if(cloned->left!=NULL)
+        {getTargetCopy(original->left, cloned->left, target);}
+        if(/*cloned->val == target->val && */ (original == target))
+        {
+            result = cloned;
+        }
+        if(cloned->right!=NULL)
+        {getTargetCopy(original->right, cloned->right, target);}
+        return result;
+    }
+};
+
+//SOLUTION WHEN NO REPEATED VALUES
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
 
 class Solution {
 public:
@@ -26,3 +55,4 @@ public:
         return result;
     }
 };
+*/
