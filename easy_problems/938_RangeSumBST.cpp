7fe8828a50e56a3598e8ffa33f9f3df2d5016e05
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-04 05:56:51 -0500

    Create 938_RangeSumBST.cpp
    Code works but cannot use global variable because that makes the recursive function a one time use only.

---
+++ easy_problems/938_RangeSumBST.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int sum = 0;
+class Solution {
+public:
+    //Taken IsBound's concept from Google Search and modified to fit
+    bool IsBound(const int value, const int low, const int high) 
+    {
+        return (!(value < low) && !(high < value));
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        
+        if(root->left!=nullptr)
+        {rangeSumBST(root->left, low, high);}
+
+        if(IsBound(root->val,low,high))
+        {
+            cout<<root->val<<" ";
+            sum+=root->val;
+        }
+
+        if(root->right!=nullptr)
+        {rangeSumBST(root->right, low, high);}
+        return sum;
+    }
+};
