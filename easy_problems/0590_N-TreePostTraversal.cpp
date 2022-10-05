1fe2377e2be29ca149f50238d45ca6d1c685ec1d
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-05 02:20:48 -0500

    Create 0590_N-TreePostTraversal.cpp
    This is the code to get some insight on how the question might be solved. Its too late to continue hence saving only this much for now.

---
+++ easy_problems/0590_N-TreePostTraversal.cpp
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorder(Node* root) {
+        cout<<root->val;
+        cout<<root->children[1]->val;
+        return{1};
+    }
+};
