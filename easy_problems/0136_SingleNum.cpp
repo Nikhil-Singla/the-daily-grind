baa51331383f71be2ce08f60b8c57ab1be7ea687
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-06 20:24:32 -0500

    Added the optimized XOR solution

--- easy_problems/0136_SingleNum.cpp
+++ easy_problems/0136_SingleNum.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    int singleNumber(vector<int>& nums) {
+        int xor1=nums[0];
+        for(int i=0;i<nums.size();i++){
+            xor1=xor1^nums[i];
+        }
+        return xor1;
+    }
+};
+
+class Solution {
+public:
     int singleNumber(vector<int>& nums) 
     {
         unordered_map<int, int>hash;
