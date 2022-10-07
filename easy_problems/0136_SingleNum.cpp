0f93e269ae9c7f1cddac3ba3a7d0d9c4fbb4e91f
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-06 20:22:47 -0500

    Create 0136_SingleNum.cpp
    Really obvious hashmap solution to run in time O(n)

---
+++ easy_problems/0136_SingleNum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        unordered_map<int, int>hash;
+        for(int i = 0; i<nums.size();i++)
+        {
+            if(hash[nums[i]]==1)
+            {
+                hash[nums[i]] = -1;
+            }
+            hash[nums[i]]++; 
+        }
+        for(int i = 0; i<nums.size();i++)
+        {
+            if(hash[nums[i]]==1)
+            {
+                return nums[i];
+            }
+        }
+        return nums[0];
+
+    }
+};
