42943ff4a52378f9e4a8594ba1055529fdeba42b
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-09 21:41:20 -0500

    First trial which completed testcases.

---
+++ easy_problems/0009_palindromeNumber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        vector<int> num;
+        int count = 0, temp = x;
+        if(x < 0)
+        {
+            return false;
+        }
+        while(temp != 0)
+        {
+            count *= 10;
+            count += (temp%10);
+            temp /= 10;
+            // cout<<count<<" ";
+        }
+        if(count == x)
+        {return true;}
+        else
+        {return false;}
+    }
+};
