d5434bac132d4445762238e39002fab0b4b04f5f
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-10 21:25:03 -0500

    First code to pass default test cases

---
+++ easy_problems/0104_MaxDepth.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr)
+        {return 0;}
+        return max(maxDepth(root->left, 1),maxDepth(root->right, 1));
+    }
+    int maxDepth(TreeNode* child, int depth)
+    {
+        if(child == nullptr)
+        {return depth;}
+        return max(maxDepth(child->left, depth+1),maxDepth(child->right, depth+1));
+    }
+};
