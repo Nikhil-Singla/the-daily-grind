9fa67aa6aef0f07885c7b787e5686fecc872cf63
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-11 21:43:59 -0500

    Very minor logical inconsistency

--- easy_problems/0217_ContainsDuplicate.cpp
+++ easy_problems/0217_ContainsDuplicate.cpp
@@ -3,7 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int>hash;
         if(nums.size()==1)
-        {return true;}
+        {return false;}
         for(int i=0; i<nums.size(); i++)
         {
             if(hash[nums[i]] == 1)
