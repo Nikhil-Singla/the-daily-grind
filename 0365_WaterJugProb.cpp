b572c6bdf2d59b2700fac50ca45a25cc3540ac77
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2022-10-11 23:59:53 -0500

    Added starting comments

--- 0365_WaterJugProb.cpp
+++ 0365_WaterJugProb.cpp
@@ -1,6 +1,8 @@
 /*
+Question:
+
 You are given two jugs with capacities jug1Capacity and jug2Capacity liters. There is an infinite amount of water supply available. Determine whether it is 
-<br />possible to measure exactly targetCapacity liters using these two jugs.
+possible to measure exactly targetCapacity liters using these two jugs.
 
 If targetCapacity liters of water are measurable, you must have targetCapacity liters of water contained within one or both buckets by the end.
 
@@ -17,13 +19,15 @@ public:
     bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) 
     {
         
-        int x=jug1Capacity,y=jug2Capacity,z=x+y;
-        int steps[]={x,-x,-y,y}; //STEPS THAT CAN BE PERFORMED
+        int x=jug1Capacity,y=jug2Capacity,z=x+y; //: X is Jug 1 Capacity, Y is Jug 2 Capacity and Z is the Total Sum of capacities.
+        int steps[]={x,-x,-y,y}; //STEPS THAT CAN BE PERFORMED: //: The possible individual moves that can be taken from the two jugs
         
-        queue<int> q;
-        vector<int> vis(z+1,0); // VISITED ARRAY TO KEEP NOT ITERATING THE SAME VALUES OVER AND OVER AGAIN
-        q.push(0);
-        vis[0]=1;
+        queue<int> q; //: Declaring a queue
+        vector<int> vis(z+1,0); // VISITED ARRAY TO KEEP NOT ITERATING THE SAME VALUES OVER AND OVER AGAIN 
+        //: Vector array initialized to the size of sum of the two jugs + 1, all initialised to 0.
+        
+        q.push(0); // Pushing the initial water cap in the jug?
+        vis[0]=1; // Visited 0
         while(!q.empty())
         {
             int node=q.front();
