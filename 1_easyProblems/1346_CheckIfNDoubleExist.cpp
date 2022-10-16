86e64e2aa03cb264f5874f3028e397d82f90dfb2
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-15 21:01:23 -0500

    Zero condition added

--- 1_easyProblems/1346_CheckIfNDoubleExist.cpp
+++ 1_easyProblems/1346_CheckIfNDoubleExist.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
         unordered_map<int, int> hash;
+        int flag = -1;
         for(int i = 0; i<arr.size(); i++)
         {
             hash[arr[i]] += 1;
+            if(arr[i] ==  0)
+            {
+                flag = i;
+            }
         }
         for(int i = 0; i<arr.size(); i++)
         {
-            if(hash[2*arr[i]] > 0)
+
+            if(hash[2*arr[i]] > 0 && (flag != i))
             {
                 return true;
             }
