d70404902ff253f2f4aa143ac42e32d4edf70547
Nikhil-Singla <nsingla3.14@gmail.com>
2022-10-15 21:46:35 -0500

    Alternate Less memory intensive solution

--- 1_easyProblems/1346_CheckIfNDoubleExist.cpp
+++ 1_easyProblems/1346_CheckIfNDoubleExist.cpp
@@ -22,3 +22,21 @@ public:
         return false;
     }
 };
+
+// Alternate Solution: BRUTE FORCE
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) 
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            for(int j = i+1; j < arr.size(); j++)
+            {
+                if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i])
+                {return 1;}
+            }
+        }
+    return 0;
+    }
+};
