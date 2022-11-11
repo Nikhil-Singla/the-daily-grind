cc894a93b1113adb954dfda27653623cd9646ded
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-10 23:59:27 -0600

    Next try

--- 2_mediumProblems/0006_ZigZag.cpp
+++ 2_mediumProblems/0006_ZigZag.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
+
+    void init
+
     string convert(string s, int numRows) 
     {
         if(numRows == 1)
         {
             return s;
         }
-
-        int skipper = (numRows-1) * 2; // How many elements we skip per letter
-        int evenSkipper = numRows/2;
-        int index = 0;
-        int loopNum = 1;
         int len = s.size();
-        string result;
-
-        for(int counter = 0; counter < len; counter++)
+        char ans[numRows][len/2];
+        
+        init(char);
+        
+        for(int counter = 0; counter<len; counter++)
         {
-            if(index > len-1)
-            {
-                index = loopNum++;
-            }
-            result.push_back(s[index]);
-            if(loopNum%2 == 1)
-            {
-                index += skipper;
-            }
-            else
-            {
-                index += evenSkipper;
-            }
+            
         }
-        return result;
     }
 };
