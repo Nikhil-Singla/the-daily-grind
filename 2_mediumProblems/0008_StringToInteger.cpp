27a982f695843e5d534054f12849734f2f85c818
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-10 22:44:42 -0600

    Got everything working except negation

--- 2_mediumProblems/0008_StringToInteger.cpp
+++ 2_mediumProblems/0008_StringToInteger.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
+
+    bool isNum(char a)
+    {
+        if(a < 58 && a>47)
+        {
+            return true;
+        }
+        return false;
+    }
+
     int myAtoi(string s) 
     {
         int len = s.size();
-        int result = 0;
+        long unsigned result = 0;
         int flag = 0;
         bool negate = false;
-        int insert = 0;
-        for(int i = 0; i < len; i++)
+        int insert = 0, i = 0;
+        long unsigned low = 0, high = 2147483647, higher = 2147483648;
+        while(s[i] == ' ')
         {
-            while(s[i] == ' ')
-            {
-                i++;
-            }
-            if(s[i] == '-')
-            {
-                negate = true;
-            }
-            else
-            {
-                while(s[i] < 58 && s[i]>47)
-                {
-                    insert = s[i] - 47;
-                    result *= 10;
-                    result += insert;
-                    i++;
-                }
-            }
+            i++;
+        }
+        if(s[i] == '-')
+        {
+            negate = true;
+            i++;
+        }
+        while(isNum(s[i]))
+        {
+            insert = s[i] - 48;
+            result *= 10;
+            result += insert;
+            i++;
         }
         if(negate)
         {
-            result *= 1;
+            result = clamp(result, low, higher);
+            insert = result;
+            insert *= -1;
         }
+        else
+        {
+            result = clamp(result, low, high);
+        }
+        cout<<negate;
         return result;
     }
 };
