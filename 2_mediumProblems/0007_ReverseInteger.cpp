9112d9206713315fbebcd47c1a64af2614501379
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-21 23:36:20 -0600

    Commenting for clarity

--- 2_mediumProblems/0007_ReverseInteger.cpp
+++ 2_mediumProblems/0007_ReverseInteger.cpp
@@ -3,29 +3,29 @@ public:
 
     int reverse(int x) 
     {
-        if(x == INT_MIN) 
+        if(x == INT_MIN)                            // Min length number, auto reverse to zero 
         {
             return 0;
         }
         
         if(x < 0) 
         {
-            return (reverse(-x)*-1);
+            return (reverse(-x)*-1);                //If x is negative, call it with a positive and add negative at the end, during return.
         }
 
-        int result = 0;
+        int result = 0;                             //Initialize return to 0
         
-        while(x > 0)
+        while(x > 0)                                //Exit condition otherwise we keep reversing digit by digit
         {
-            int a = x % 10;
-            x = x / 10;
-            if( (INT_MAX - a)/10 < result ) 
-            {
-                return 0;
+            int a = x % 10;                         //Get the units digit from x
+            x = x / 10;                             //Change the value of x
+            if( (INT_MAX - a)/10 < result )         //To prevent overflow, we check if the digit can be subtracted and divided by 10 from Max Possible. If not, then it returns 0
+            {   
+                return 0;                           //Return 0 if overflow
             }
-            result = result * 10 + a;
+            result = result * 10 + a;               //Add the units digit to the result which is reversing
         }
-        return result;
+        return result;                              //Return the correct result
     }
 
 };
