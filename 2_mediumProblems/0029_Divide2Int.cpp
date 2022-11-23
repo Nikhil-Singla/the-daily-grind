afb1abd5b62f31ab97cced885235b972a3a65563
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-22 20:52:15 -0600

    Clear Commenting

--- 2_mediumProblems/0029_Divide2Int.cpp
+++ 2_mediumProblems/0029_Divide2Int.cpp
@@ -1,61 +1,65 @@
+//Program to manually perform divison for 2 integers.
 class Solution {
 public:
 
-    bool sign(int dividend, int divisor)
-    {
-        bool negate = false;
-        if(dividend < 0)
+    bool sign(int dividend, int divisor)        //Function to return the sign of the final answer based on 
+    {                                           //divisor and dividend. True = answer is negative 
+        bool negate = false;                    //Initialize
+        if(dividend < 0)                        //If Dividend is negative
         {
-            if(divisor < 0)
-            {}
+            if(divisor < 0)                     //And divisor is also negative
+            {}                                  //Negate stays false, ie result is positive
             else
-            {negate = true;}
+            {negate = true;}                    //Otherwise result is negative
         }
-        else
+        else                                    //If Dividend is positive
         {
-            if(divisor >= 0)
-            {}
+            if(divisor >= 0)                    //And divisor is positive
+            {}                                  //Negate stays false and result is positive
             else
-            {negate = true;}
+            {negate = true;}                    //Otherwise result is negative
         }    
-        return negate;
+        return negate;                          //Return negate
     }
 
     int divide(int dividend, int divisor) 
     {
-        bool negate = false;
-        int result = 0;
+        bool negate = false;                    //Sign checker
+        int result = 0;                         //Store answer here
         
-        negate = sign(dividend, divisor);
+        negate = sign(dividend, divisor);       //Get our initial signs
         
-        if(dividend == INT_MIN)
-        {
+        if(dividend == INT_MIN)                 //Smallest number case because we cannot store it otherwise in absolute
+        {                                       //function as we are converting everything to positive
             if(divisor > 0)
             {
-                dividend += divisor;
-                result++;
+                dividend += divisor;            //Division is nothing but taking away divisor from dividend
+                result++;                       //Here dividend is negative(min) and hence we add the positive divisor
             }
             else
             {
-                dividend -= divisor;
+                dividend -= divisor;            //And subtract the negative divisor 
                 result++;
             }
         }
-        dividend = abs(dividend);
+
+        dividend = abs(dividend);               //Convert to absolute values to make calc easier
         divisor = abs(divisor);
 
-        for(; (dividend >= divisor) && (divisor!= 0); result++)
-        {
-            if(result == INT_MAX)
-            {return (negate?INT_MIN:INT_MAX);}
-            dividend -= divisor;
+        for(; (dividend >= divisor) && (divisor!= 0); result++)     //Keep going till dividend is bigger than divisor as any less and we reach the remainder
+        {                                                           //Make sure divisor isn't negative, and every time we take it away, we add 1 to the quotient
+            if(result == INT_MAX)                                   //If we ever hit the max limit
+            {
+                return ( negate ? INT_MIN : INT_MAX );              //Check if we have negative, if we do, return Min possible value, otherwise Max possible
+            }                                                       
+            dividend -= divisor;                                    //Else we keep performing division by taking away divisor from dividend
         }    
 
-        if(negate)
+        if(negate)                              //After exit loop, check if sign is negative during return
         {
             result *= -1;
         }
 
-        return result;
+        return result;                          //Else return the correct answer
     }
 };
