79b8a7324a45b53a16f38d301d0a84822ba6f19b
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-22 20:06:21 -0600

    Commenting

--- 2_mediumProblems/0008_StringToInteger.cpp
+++ 2_mediumProblems/0008_StringToInteger.cpp
@@ -15,42 +15,42 @@ public:
         int i = 0;              //Keep track of the current input character from the entire string
         bool negate = false;    //Keep track if the number is negative or not
 
-        while(s[i] == ' ')
+        while(s[i] == ' ')      //If the string has spaces in the beginning, keep moving the index pointer forward
         {
             i++;
         }
-        if(s[i] == '-')
+        if(s[i] == '-')         //If we find the negative sign, the final answer has to be negative.
         {
             negate = true;
             i++;
         }
-        else if(s[i] == '+')
+        else if(s[i] == '+')    //If we find the positive sign, ignore it
         {
             i++;
         }
-        while(isNum(s[i]))
+        while(isNum(s[i]))      //If the index after is a number
         {
-            result = result*10 + (s[i] - 48);
+            result = result*10 + (s[i] - 48);   //Add the index to result
             i++;
-            if(result >= INT_MAX) 
-            {
-                break;
+            if(result >= INT_MAX)               //If result exceeds the max value, we can stop as any further
+            {                                   //Further digits will only make the answer larger and we need 
+                break;                          //to clamp the result
             }   
         }
 
-        if(negate) 
+        if(negate)                              //Check if the answer is supposed to be negative or not
         {
             result *= -1;
         }
-        if(result <= INT_MIN) 
+        if(result <= INT_MIN)                   //Clamp from the lower side
         {
             return INT_MIN;
         }
-        if(result >= INT_MAX) 
+        if(result >= INT_MAX)                   //Clamp from the upper side
         {
             return INT_MAX;
         }
 
-        return result;
+        return result;                          //If not clamping, our result remains unchanged
     }
 };
