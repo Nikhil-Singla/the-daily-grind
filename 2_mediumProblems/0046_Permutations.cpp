af6f4c100e59478d98f6b6221a3a4cd051c6c241
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-25 18:40:01 -0600

    Complete code

--- 2_mediumProblems/0046_Permutations.cpp
+++ 2_mediumProblems/0046_Permutations.cpp
@@ -18,17 +18,20 @@ public:
 
     void permute(vector<int>& nums, int left_pointer, int right_pointer)
     {
-        if (left_pointer == right_pointer)
+        if (left_pointer == right_pointer)  //If our left and right pointers overlap, then all possible swapping is finished
         {
-            ans.push_back(nums);
+            ans.push_back(nums);            //We can push back the current state of our nums array to our final answer
         }
         else 
         {
-            for (int index = left_pointer; index <= right_pointer; index++) 
+            for (int index = left_pointer; index <= right_pointer; index++)     //If indexes don't overlap
             {
-                swap(nums[left_pointer], nums[index]);
-                permute(nums, left_pointer + 1, right_pointer);
-                swap(nums[left_pointer], nums[index]);
+                swap(nums[left_pointer], nums[index]);                          //Swap the left pointer with the index to get one permutation
+                permute(nums, left_pointer + 1, right_pointer);                 //Recursively call the function with the next swap
+                swap(nums[left_pointer], nums[index]);                          //Backtrack the swap that we have done
+            
+                //Here since we do the swapping first, we store the answer as we're undoing all the swappings. ie, we first find our result after the final swap
+                //and then we keep appending them as we are also simultaneously undoing our swaps afterwards
             }
         }
     }
