83e9360768867609bd30949728258e91e3ba70d5
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-26 20:31:18 -0600

    Hash Map solution

--- 1_easyProblems/0160_IntersectionLinkedList.cpp
+++ 1_easyProblems/0160_IntersectionLinkedList.cpp
@@ -10,6 +10,30 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
     {
+        unordered_map<ListNode*, int> hash_map;
+        while(headB != NULL)
+        {
+            hash_map[headB]++;
+            headB = headB->next;
+        }
+        while(headA != NULL)
+        {
+            if(hash_map[headA] == 1)
+            {
+                return headA;
+            }
+            headA = headA->next;
+        }
+        
+        return NULL;
+    }
+};
+
+// ACCEPTED BRUTE FORCE APPROACH
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
         ListNode* temp;
         while(headA != NULL)
         {
