70eb6241464cc61669bf17da2a6ee64a57850113
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-26 20:14:58 -0600

    Solution passing testcases.

--- 1_easyProblems/0122_BuyandSellStock2.cpp
+++ 1_easyProblems/0122_BuyandSellStock2.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int leastPrice = prices[0];
-        int currentProfit = 0;
         int maxProfit = 0;
-        int cycleProfit = 0;
-        for(int i = 0; i < prices.size(); i++)
+        for(int i = 1; i < prices.size(); i++)
         {
-            if(prices[i] < leastPrice)
+            if(prices[i] > prices[i-1])
             {
-                leastPrice = prices[i];
-                maxProfit += cycleProfit; 
-                cycleProfit = 0;
-            }
-            currentProfit = prices[i] - leastPrice;
-            if(currentProfit > cycleProfit)
-            {
-                cycleProfit = currentProfit;
+                maxProfit += prices[i] - prices[i-1];
             }
         }
         return maxProfit;
