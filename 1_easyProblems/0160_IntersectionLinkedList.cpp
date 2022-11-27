3312a7801cdc33ac1a13caf0d1af2a96c794de29
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-26 20:39:04 -0600

    Optional Solution

--- 1_easyProblems/0160_IntersectionLinkedList.cpp
+++ 1_easyProblems/0160_IntersectionLinkedList.cpp
@@ -53,3 +53,53 @@ public:
         return NULL;
     }
 };
+
+//Using solution to find the difference between the lists.
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+    {
+        int lenA = 0;
+        int lenB = 0;
+        ListNode* tempA = headA;
+        ListNode* tempB = headB;
+        while(tempA != NULL)
+        {
+            lenA++;
+            tempA = tempA->next;
+        }
+        while(tempB != NULL)
+        {
+            lenB++;
+            tempB = tempB->next;
+        }
+        int diff = abs(lenA - lenB);
+        if(lenA > lenB)
+        {
+            while(diff)
+            {
+                headA = headA->next;
+                diff--;
+            }
+        }
+        else
+        {
+            while(diff)
+            {
+                headB = headB->next;
+                diff--;
+            }
+        }
+        while(headA != NULL)
+        {
+            if(headA == headB)
+            {
+                return headA;
+            }
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
+
