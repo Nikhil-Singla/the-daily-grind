b610e0ca14d2bd9ad73fcb7f7a16f4cf1ff2214e
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-27 22:56:19 -0600

    Comments with explanation

--- 2_mediumProblems/0237_DeleteNode.cpp
+++ 2_mediumProblems/0237_DeleteNode.cpp
@@ -10,9 +10,10 @@ class Solution {
 public:
     void deleteNode(ListNode* node) 
     {
-        ListNode *temp = node->next; 
-        node->val = node->next->val; 
-        node->next = node->next->next; 
-        delete temp; 
+        //We will solve this by bringing the next node to the current node and then deleting the next node.
+        ListNode *temp = node->next; //Copy the link of the next node to delete later and free memory
+        node->val = node->next->val; //Copy the value of the next node to this node
+        node->next = node->next->next; //Overwrite the next node link from this node to the node after
+        delete temp; //Free up the memory space
     }
 };
