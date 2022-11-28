e9c3841bca631ea760010d71a013b62bb026df03
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-27 23:33:01 -0600

    Solution to test myself

--- 1_easyProblems/0026_RemoveDuplicates.cpp
+++ 1_easyProblems/0026_RemoveDuplicates.cpp
@@ -1,3 +1,16 @@
+
+
+// Copied solution to study further
+int removeDuplicates(vector<int>& nums) {
+    int i = 0;
+    for (int n : nums)
+        if (!i || n > nums[i-1])
+            nums[i++] = n;
+    return i;
+}
+
+
+// BRUTE FORCE SOLUTION
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) 
