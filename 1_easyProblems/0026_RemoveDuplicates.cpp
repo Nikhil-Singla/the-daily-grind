711190e5f004aea0471e11df33e6a487d7ea60bc
Nikhil-Singla <nsingla3.14@gmail.com>
2022-11-27 23:34:17 -0600

    Optimal solution using inplace modification

--- 1_easyProblems/0026_RemoveDuplicates.cpp
+++ 1_easyProblems/0026_RemoveDuplicates.cpp
@@ -1,4 +1,18 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int k = 1;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] != nums[i-1])
+            {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+};
 
 // Copied solution to study further
 int removeDuplicates(vector<int>& nums) {
