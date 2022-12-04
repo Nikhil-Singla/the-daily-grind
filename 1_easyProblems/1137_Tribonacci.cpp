3968ed32b93666cfe72c00f9dcb802d55c4f27b4
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-04 07:18:36 -0600

    Code passed testcases

---
+++ 1_easyProblems/1137_Tribonacci.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int tribonacci(int n) 
+    {
+        vector<int> ans;
+        ans.push_back(0);
+        ans.push_back(1);
+        ans.push_back(1);
+        for(auto i = 3; i <= n; i++)
+        {
+            ans.push_back(ans[i-1] + ans[i-2] + ans[i-3]);
+        }    
+        return ans[n];
+    }
+};
