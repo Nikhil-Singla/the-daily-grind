e489f32f0d39b757cee03cc7fc50e012a38810d2
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-04 07:08:23 -0600

    Improved

--- 1_easyProblems/0509_FibNumbers.cpp
+++ 1_easyProblems/0509_FibNumbers.cpp
@@ -1,3 +1,24 @@
+// MUCH BETTER: Iterative Solution
+class Solution {
+public:
+    int fib(int n) 
+    {
+        vector<int> ans;
+        if(n < 2)
+        {
+            return n;
+        }
+        ans.push_back(0);
+        ans.push_back(1);
+        for(int i = 2; i <= n; i++)
+        {
+            ans.push_back(ans[i-1] + ans[i-2]);
+        }
+        return ans[n];
+    }
+};
+
+// Recursive Solution
 class Solution {
 public:
     int fib(int n) 
