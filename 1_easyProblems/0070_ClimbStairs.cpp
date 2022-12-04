7a603c96367b57a5364a5e3dc998abbd21d85060
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-04 07:48:02 -0600

    Alternatives

--- 1_easyProblems/0070_ClimbStairs.cpp
+++ 1_easyProblems/0070_ClimbStairs.cpp
@@ -1,10 +1,13 @@
+// Quickest Solution
 class Solution {
 public:
 
     int climbStairs(int n) 
     {
         vector<int> ans(n+1, 0);
+
         // base cases
+        
         if(n <= 0) return 0;
         if(n == 1) return 1;
         if(n == 2) return 2;
@@ -17,6 +20,43 @@ public:
         {
             ans[i] = ans[i-1] + ans[i-2];
         }
+        return ans[n];
+    }
+};
+
+// Alternate Method
+class Solution {
+public:
+
+    int climbStairs(int n) 
+    {
+        vector<int> ans;
+        ans.push_back(0);
+        ans.push_back(1);
+        ans.push_back(2);
+        for(int i = 3; i <= n; i++)
+        {
+            ans.push_back(ans[i-1] + ans[i-2]);
+        }    
+        return ans[n];
+    }
+};
+
+// Slower due to one error in solution
+class Solution {
+public:
+
+    vector<int> ans;
+    
+    int climbStairs(int n) 
+    {
+        ans.push_back(0);
+        ans.push_back(1);
+        ans.push_back(2);
+        for(int i = 3; i <= n; i++)
+        {
+            ans.push_back(ans[i-1] + ans[i-2]);
+        }    
         return ans[n];
     }
 };
