196057fc53b742a07ef1d752505ce6199687b530
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-17 17:16:02 -0600

    Commenting

--- 1_easyProblems/1716_MoneyLeetcode.cpp
+++ 1_easyProblems/1716_MoneyLeetcode.cpp
@@ -1,18 +1,19 @@
+// Code to calculate the money in the bank
 class Solution {
 public:
     int totalMoney(int n) 
     {
-        int result = 0;
-        int week = 1;
-        int day = 0;
-        for(int i = 0; i < n; i++, day++)    
+        int result = 0;     //Variable used to store the final amount
+        int week = 1;       //What week we are on
+        int day = 0;        //What day of the week we are on
+        for(int i = 0; i < n; i++, day++)   //Loop to calculate n times.
         {
-            result += (week + day);
+            result += (week + day);         //Add the money to the result
             cout<<week+day<<" ";
-            if( day%6 == 0 && day !=0)
+            if( day%6 == 0 && day !=0)      //Reset the week
             {
-                day = -1;
-                week++;
+                day = -1;                   //Start from -1 because day++
+                week++;                     //Week changes to next one
             }
         }
         return result;
