1d24ad24ed76a9b27bee97fc460e9bd5ccffc1f4
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-18 14:18:34 -0600

    First code that passed testcases

---
+++ 1_easyProblems/0442_Duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) 
+    {
+        vector<int> result;
+        for(int i = 0; i<nums.size(); ++i)
+        {
+            int temp = abs(nums[i]) - 1;
+            if(nums[temp] < 0)
+            {
+                result.push_back(temp+1);
+                cout<<temp<<" ";
+            } 
+            nums[temp] *= -1;
+        }     
+        return result;
+    }
+};
