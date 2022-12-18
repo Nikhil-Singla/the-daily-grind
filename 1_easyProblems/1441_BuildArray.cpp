116dc8f8ff551fe940003d0f4812a1f86dc8608c
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-18 15:20:10 -0600

    Removing redundancy. Passed testcases

--- 1_easyProblems/1441_BuildArray.cpp
+++ 1_easyProblems/1441_BuildArray.cpp
@@ -5,22 +5,17 @@ public:
         vector<string> result;
         int stream_counter = 1;
         int i = 0;
-        int pop_counter = 0;
         while(i < target.size())
         {   
             if(stream_counter == target[i])
             {
-                while(pop_counter > 0)
-                {
-                    result.push_back("Pop");
-                }
                 result.push_back("Push");
                 i++;
             }
             else
             {
                 result.push_back("Push");
-                pop_counter++;
+                result.push_back("Pop");
             }
             stream_counter++;
         }   
