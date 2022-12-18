3a852c15ad022553115e79357fa70823ea5cc0c6
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-18 14:23:05 -0600

    First code that passed the testcases

---
+++ 1_easyProblems/0448_FindDisappeared.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) 
+    {
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int temp = abs(nums[i]) - 1;
+            if(nums[temp] > 0)
+            {
+                nums[temp] *= -1;
+            }
+        }    
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            {
+                result.push_back(i+1);
+            }
+        }
+        return result;
+    }
+};
