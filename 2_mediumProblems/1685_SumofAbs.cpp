7a8a1e4483a7a8c3db007060704726bd194825df
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-19 16:17:41 -0600

    Even I was surprised I did this.

---
+++ 2_mediumProblems/1685_SumofAbs.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> getSumAbsoluteDifferences(vector<int>& nums) 
+    {
+        vector<int> result;
+        result.push_back(0);
+        int n = nums.size();
+        for(int i = 1; i<n; i++)
+        {
+            result[0] += nums[i] - nums[0];
+        }    
+        for(int i = 1; i < n; i++)
+        {
+            int diff = nums[i] - nums[i-1];
+            int add = (i)*diff;
+            int sub = (n-i)*diff;
+            result.push_back(result[i-1] + add - sub);
+        }
+        return result;
+    }
+};
