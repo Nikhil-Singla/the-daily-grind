817e5f1accd3b4363db92123cae01dfff0fe03bc
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-27 18:02:46 -0600

    Testing optimization

--- 1_easyProblems/0349_IntersectionArray.cpp
+++ 1_easyProblems/0349_IntersectionArray.cpp
@@ -20,3 +20,26 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        unordered_map<int, int> hash;
+        vector<int> ans;
+        unordered_set<int> unique;
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            hash[nums1[i]]++;
+        }    
+        for(int i = 0; i < nums2.size(); i++)
+        {
+            if(hash[nums2[i]] > 0)
+            {
+                unique.insert(nums2[i]);
+            }
+        }
+        ans.assign( unique.begin(), unique.end() );
+        return ans;
+    }
+};
