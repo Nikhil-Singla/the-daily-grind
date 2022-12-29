ca2095fd56dbe9710d1ceb5752560b93f03e7eea
Nikhil-Singla <nsingla3.14@gmail.com>
2022-12-29 13:13:49 -0600

    DIGIT ROOT SOLUTION

--- 1_easyProblems/0258_AddDigits.cpp
+++ 1_easyProblems/0258_AddDigits.cpp
@@ -2,6 +2,14 @@ class Solution {
 public:
     int addDigits(int num) 
     {
+        return (num==0) ? 0 : ( (num%9==0) ? 9 : num%9 );
+    }
+};
+
+class Solution {
+public:
+    int addDigits(int num) 
+    {
         while(num > 9)
         {
             int temp = 0;
