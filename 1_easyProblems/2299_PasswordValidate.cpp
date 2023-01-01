46f077c3f174226993972c0820c9839b4b295978
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-01 16:07:20 -0600

    Quicker version of the code

--- 1_easyProblems/2299_PasswordValidate.cpp
+++ 1_easyProblems/2299_PasswordValidate.cpp
@@ -2,6 +2,68 @@ class Solution {
 public:
     bool strongPasswordCheckerII(string password) 
     {
+        string special = "!@#$%^&*()-+";
+        bool check[5] = {false};
+        if(password.length() < 8)
+        {return false;}
+
+        for(int i = 0; i < password.length(); i++)
+        {
+            if(password[i] >= 'a' && password[i] <= 'z')
+            {
+                check[0] = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < password.length(); i++)
+        {
+            if(password[i] >= 'A' && password[i] <= 'Z')
+            {
+                check[1] = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < password.length(); i++)
+        {
+            if(password[i] >= '0' && password[i] <= '9')
+            {
+                check[2] = true;
+                break;
+            }
+        }
+
+        for(int i = 0; i < password.length() && !check[3]; i++)
+        {
+            for(auto &c: special)
+            {
+                if(password[i] == c)
+                {
+                    check[3] = true;
+                    break;
+                }
+            }
+        }
+
+        check[4] = true;
+        for(int i=1; i<password.size(); i++)
+        {
+            if(password[i-1]==password[i])
+            {
+                check[4] = false;
+                break;
+            }
+        }
+
+        return(check[0] && check[1] && check[2] && check[3] && check[4]);
+    }
+};
+
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) 
+    {
         int len = password.length();
 
         if(len < 8)
