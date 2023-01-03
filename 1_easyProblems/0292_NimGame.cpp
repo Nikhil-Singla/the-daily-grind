2daeb59d936ec07ff0a56424c866260a44717a1b
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-03 16:48:22 -0600

    Quicker solution (math property)

--- 1_easyProblems/0292_NimGame.cpp
+++ 1_easyProblems/0292_NimGame.cpp
@@ -1,3 +1,13 @@
+// Its faster to resolve if-else conditions manually and instruct what to return
+// Than it is to return the if-else result.
+class Solution {
+public:
+    bool canWinNim(int n) 
+    {
+        return (n & 3 ? true : false);    
+    }
+};
+
 class Solution {
 public:
     bool canWinNim(int n) 
