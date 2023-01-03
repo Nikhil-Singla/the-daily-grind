d172a707670eb85201e79232a653c5517feb7d99
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-03 16:19:18 -0600

    Quicker solution accepted (array sum)

--- 1_easyProblems/0268_MissingNumber.cpp
+++ 1_easyProblems/0268_MissingNumber.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) 
     {
+        uint array_Sum = 0, sum = 0, i = 0;
+        for(auto& c : nums)
+        {
+            array_Sum += c;
+            sum += i++;
+        }    
+        sum += i;
+        return sum - array_Sum;
+    }
+};
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) 
+    {
         unordered_map<int, bool> hash;
         for(auto& c : nums)
         {
