762d11da49358e56ecfc10dddc75988a61029989
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-05 19:26:09 -0600

    Changes to make the solution very fast.

--- 2_mediumProblems/1329_SortDiagonal.cpp
+++ 2_mediumProblems/1329_SortDiagonal.cpp
@@ -1,3 +1,58 @@
+//Fastest solution without changing anything but making it all one function.
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) 
+    {
+        vector<int> holder;
+        uint col = mat[0].size(), row = mat.size();
+        for(uint i = 0; i < col; i++)
+        {
+            uint temp1 = 0, temp2 = i;
+            uint i1 = 0, i2 = i;
+            while((temp1 < row) && (temp2 < col))
+            {
+                holder.push_back(mat[temp1][temp2]);
+                temp1++; 
+                temp2++;
+            }
+
+            sort(holder.begin(), holder.end());
+
+            for(auto const c : holder)
+            {
+                mat[i1][i2] = c;
+                i1++; 
+                i2++;
+            }
+            holder.clear();
+        }
+        for(uint j = 1; j < row; j++)
+        {
+            uint temp1 = j, temp2 = 0;
+            uint i1 = j, i2 = 0;
+            while((temp1 < row) && (temp2 < col))
+            {
+                holder.push_back(mat[temp1][temp2]);
+                temp1++; 
+                temp2++;
+            }
+
+            sort(holder.begin(), holder.end());
+
+            for(auto const c : holder)
+            {
+                mat[i1][i2] = c;
+                i1++; 
+                i2++;
+            }
+            holder.clear();
+        }
+
+        return mat;
+    }
+    
+};
+
 // Faster solution using vector sort.
 class Solution {
 public:
