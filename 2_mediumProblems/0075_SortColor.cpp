dd8d58b3b0ee7889d2159b2e1018f1a97558a304
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-12 17:05:35 -0600

    Added if-else variation

--- 2_mediumProblems/0075_SortColor.cpp
+++ 2_mediumProblems/0075_SortColor.cpp
@@ -22,3 +22,33 @@ public:
         }    
     }
 };
+
+//If Else statements
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int low = 0, high = nums.size()-1, itr = 0;
+        if(nums.size() < 2)
+        {
+            return;
+        }
+        while(itr <= high)
+        {
+            if(nums[itr] == 0)
+            {
+                swap(nums[low], nums[itr]);
+                low++; itr++; 
+            }
+            else if(nums[itr] == 2) 
+            {
+                swap(nums[high], nums[itr]);
+                high--;
+            }
+            else 
+            {
+                itr++; 
+            }
+        }    
+    }
+};
