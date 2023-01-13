19e6a8eaf70194e100f9e7e1cc3b8d0c8e1e0c9a
NIKHIL-SINGLA <nsingla3.14@gmail.com>
2023-01-13 10:34:33 -0600

    Defining verification manually to increase speed.

--- 1_easyProblems/0125_ValidPalindrome.cpp
+++ 1_easyProblems/0125_ValidPalindrome.cpp
@@ -1,12 +1,19 @@
 #include <cctype>
 class Solution {
 public:
+    bool isvalid(char ch)
+    {
+        if((ch>='A'&& ch<='Z')|| (ch>='a'&& ch<='z')|| (ch>='0'&& ch<='9')){
+            return true;
+        }
+        return false;
+    }
     bool isPalindrome(string s) 
     {
         string phrase;
         for(auto c : s)
         {
-            if(isalnum(tolower(c)))
+            if(isvalid(tolower(c)))
             {
                 phrase += tolower(c);
             }
