d20833b83714a0598f815e6e476ea3c12156ca39
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-15 17:22:15 -0600

    Alternate method that might give faster results

--- 1_easyProblems/0485_MaxConsec1.cpp
+++ 1_easyProblems/0485_MaxConsec1.cpp
@@ -4,6 +4,28 @@ public:
     {
         uint count = 0;
         uint ans = 0;
+        for(int const c : nums)
+        {
+            if(c == 1) {
+                count++;
+                if(ans < count)
+                {ans = count;}
+            }
+            else {
+                count = 0;
+            }
+        }    
+        ans = max(ans, count);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
+        uint count = 0;
+        uint ans = 0;
         for(short const c : nums)
         {
             if(c & 1) {count++;}
