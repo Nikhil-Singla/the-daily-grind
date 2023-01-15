6fe54f8ad4dbf1976e2929298708543a87a347ca
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-15 16:33:29 -0600

    Additional accepted answer

--- 1_easyProblems/0367_IsPerfectSq.cpp
+++ 1_easyProblems/0367_IsPerfectSq.cpp
@@ -1,3 +1,23 @@
+// Code using mathematics property
+class Solution {
+public:
+    bool isPerfectSquare(int num) 
+    {
+        long sum = 0;
+        for(uint i = 1; i <= num; i += 2)
+        {
+            sum += i;
+            if(sum < num)
+            {}
+            else
+            {
+                return sum == num;
+            }
+        }
+        return false;
+    }
+};
+
 class Solution {
 public:
     bool isPerfectSquare(int num) 
