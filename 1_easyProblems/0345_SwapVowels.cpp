c6ef43dcfab5be59993848dcac1658a0e2ff0322
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-14 19:40:53 -0600

    Delete debugging code. Compacted.

--- 1_easyProblems/0345_SwapVowels.cpp
+++ 1_easyProblems/0345_SwapVowels.cpp
@@ -9,6 +9,29 @@ public:
 
     string reverseVowels(string s) 
     {
+        int start = 0, end = s.length()-1;
+        while(start < end)
+        {
+            while(start < end && !iV(s[start])) start++;
+            while(start < end && !iV(s[end])) end--;
+            swap(s[start],s[end]);
+            start++;end--;
+        }
+        return s;
+    }
+};
+
+class Solution {
+public:
+
+    bool iV(char c)     // is Vowel function
+    {
+        char d = tolower(c);
+        return ( d=='a' || d=='e' || d=='i' || d=='o' || d=='u' );
+    }
+
+    string reverseVowels(string s) 
+    {
         int Start = 0, End = s.length()-1;
         bool flag;
         while(Start < End)
@@ -20,7 +43,6 @@ public:
                     End--;
                 }
                 if(iV(s[End]) && Start != End) { swap( s[Start], s[End--] ); }
-                cout<<"Swapping: "<<s[Start]<<" and "<<s[End]<<"\n";
             }
             Start++;
         }
