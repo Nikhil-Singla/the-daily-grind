b95268b3c29d4a82f868cd57f17f07965349bc6d
NIKHIL-SINGLA <nsingla3.14@gmail.com>
2023-01-22 15:12:24 -0600

    LogN of time

--- 1_easyProblems/0441_ArrangingCoins.cpp
+++ 1_easyProblems/0441_ArrangingCoins.cpp
@@ -2,6 +2,29 @@ class Solution {
 public:
     int arrangeCoins(int n) 
     {
+        int l = 1; // l = lefr
+        int r = n; // r = right
+
+        while(l <= r)
+        {
+            long mid = l + (r - l ) / 2;
+            if (n >= (mid * (mid + 1 ) /2 ))
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        return l-1;
+    }
+};
+
+class Solution {
+public:
+    int arrangeCoins(int n) 
+    {
         int answer = 0;
         for(int i = 1; n > 0; i++)
         {
