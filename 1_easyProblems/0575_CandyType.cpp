54e01a01775d51a7338d07f776e94053a869d000
NIKHIL-SINGLA <nsingla3.14@gmail.com>
2023-01-24 15:06:19 -0600

    much faster version accepted successfully

--- 1_easyProblems/0575_CandyType.cpp
+++ 1_easyProblems/0575_CandyType.cpp
@@ -2,6 +2,15 @@ class Solution {
 public:
     int distributeCandies(vector<int>& candyType) 
     {
+        set<int> s(candyType.begin(), candyType.end());
+        return min(candyType.size()/2, s.size());
+    }
+};
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) 
+    {
         int typesOfCandy = 0;
         unordered_map <int, bool> map;
         for(auto c : candyType)
