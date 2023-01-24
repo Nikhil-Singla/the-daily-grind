933a774331cb38c0d694c8e805fa0c20d4178273
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-23 23:34:50 -0600

    Different sorting method accepted quicker

--- 1_easyProblems/2144_MinCost.cpp
+++ 1_easyProblems/2144_MinCost.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     int minimumCost(vector<int>& cost) 
     {
+        sort(cost.begin(), cost.end(), greater<int>());
+        int sum = 0;
+        for(int i = 0; i <cost.size(); i += 3)
+        {
+            sum += cost[i];
+            if(i < cost.size()-1) {sum += cost[i+1];}
+        }    
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) 
+    {
         sort(cost.begin(), cost.end());
         int sum = 0;
         for(int i = cost.size()-1; i >=0; i -= 3)
