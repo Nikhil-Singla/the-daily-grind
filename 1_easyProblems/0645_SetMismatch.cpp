c8d6de46775d98297cb595c47facc7896f5212ee
Nikhil-Singla <nsingla3.14@gmail.com>
2023-01-26 21:52:39 -0600

    Much faster code logic

--- 1_easyProblems/0645_SetMismatch.cpp
+++ 1_easyProblems/0645_SetMismatch.cpp
@@ -2,6 +2,33 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) 
     {
+        vector<int> ans;
+        vector<bool> arr(nums.size()+1, false);
+        int n = nums.size();
+        for(uint const c : nums)
+        {
+            if(arr[c])
+            {
+                ans.push_back(c);
+            }
+            arr[c] = true;
+        }
+        for(uint i = 1; i < arr.size(); i++)
+        {
+            if(!arr[i])
+            {
+                ans.push_back(i);
+                return ans;
+            }
+        }
+        return {-1, -1};
+    }
+};
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) 
+    {
         vector<int> ans(2);
         int n = nums.size();
         int sum = (n*(n+1))/2;
