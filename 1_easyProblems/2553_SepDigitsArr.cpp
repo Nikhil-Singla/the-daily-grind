ed54089a8fc3f13b63b53bbf472d0043adaf9990
Nikhil-Singla <nsingla3.14@gmail.com>
2023-02-16 23:44:50 -0600

    Code passing and accepted successfully

--- 1_easyProblems/2553_SepDigitsArr.cpp
+++ 1_easyProblems/2553_SepDigitsArr.cpp
@@ -1,3 +1,23 @@
+// Faster code
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) 
+    {
+        vector<int> result;
+        for(auto c : nums)
+        {
+            string s = to_string(c);
+            for(auto d : s)
+            {
+                result.push_back(d - '0');
+            }
+        }
+        return result;
+    }
+};
+
+
+// Brute force solution
 class Solution {
 public:
     vector<int> separateDigits(vector<int>& nums) 
