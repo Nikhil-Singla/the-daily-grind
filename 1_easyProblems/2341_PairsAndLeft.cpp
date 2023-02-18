38405a64917e9c16af2a4ff37c0622835eba975a
Nikhil-Singla <nsingla3.14@gmail.com>
2023-02-17 23:22:46 -0600

    Faster code

--- 1_easyProblems/2341_PairsAndLeft.cpp
+++ 1_easyProblems/2341_PairsAndLeft.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) 
     {
+        unordered_map<int, int> CountOfNums;
+        for(int const c : nums)
+        {
+            CountOfNums[c]++;
+        }
+        int a = 0, b = 0;
+        for(auto const c : CountOfNums)
+        {
+            a += c.second/2;
+            b += c.second%2;
+        } 
+        return {a, b};
+    }
+};
+
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) 
+    {
         unordered_map<int, int> address;
         vector<int> res(2);
         res[0] = 0;
