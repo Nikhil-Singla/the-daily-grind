6e27b2d5edf5e00ac8a94ef233f4c2258073fee9
NIKHIL-SINGLA <nsingla3.14@gmail.com>
2023-02-27 21:04:09 -0600

    Variable reduction

--- 1_easyProblems/0495_Teemo.cpp
+++ 1_easyProblems/0495_Teemo.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int findPoisonedDuration(vector<int>& tS, int duration) 
+    int findPoisonedDuration(vector<int>& tS, int d) 
     {
         int seconds = 0;
         for(int i = 0 ; i < tS.size()-1; i++)
         {
-            if(tS[i] + duration > tS[i+1])
+            if(tS[i] + d > tS[i+1])
             {
                 seconds += tS[i+1] - tS[i];
             }
             else
             {
-                seconds += duration;
+                seconds += d;
             }
         }
-        return seconds+duration;
+        return seconds+d;
     }
 };
