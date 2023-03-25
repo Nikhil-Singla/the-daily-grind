009e7914319d43c33a2aa41d7bb2963bd88e3a53
Nikhil-Singla <nsingla3.14@gmail.com>
2023-03-24 23:38:12 -0500

    Code commented

--- Cpp_and_Python/1_easyProblems/0014_LongPreStr.cpp
+++ Cpp_and_Python/1_easyProblems/0014_LongPreStr.cpp
@@ -1,21 +1,40 @@
+/** 
+The longestCommonPrefix function takes a vector of strings as input and returns the longest common prefix of all the strings.
+The function first sorts the input vector in ascending order using the sort function from the standard library. 
+It then determines the number of strings in the input vector and initializes an empty string ans for the common prefix.
+The function then iterates over each character in the first and last strings of the sorted vector. If the current character is the same in both strings, 
+it is added to the common prefix. If the current character is different, the loop is broken and the function returns the common prefix.
+**/
+
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) 
     {
+        // Sort the input vector of strings in ascending order
         sort(strs.begin(), strs.end());
+
+        // Determine the number of strings in the input vector
         uint n = strs.size();
+
+        // Initialize an empty string for the common prefix
         string ans = "";
+
+        // Iterate over each character in the first and last strings of the sorted vector
         for(int i = 0; (i < strs[n-1].length()) && (i < strs[0].length()); i++)
         {
+            // If the current character is the same in both strings, add it to the common prefix
             if(strs[n-1][i] == strs[0][i])
             {
                 ans += strs[n-1][i];
             }
+            // Otherwise, break out of the loop since the common prefix has ended
             else
             {
                 break;
             }
         }    
+
+        // Return the common prefix
         return ans;
     }
 };
