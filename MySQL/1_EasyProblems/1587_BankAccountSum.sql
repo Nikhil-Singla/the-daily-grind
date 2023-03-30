73b734061e8aca13b2398ee4c03ad5644a09a951
Nikhil-Singla <nsingla3.14@gmail.com>
2023-03-29 22:04:23 -0500

    Added fastest solution

--- MySQL/1_EasyProblems/1587_BankAccountSum.sql
+++ MySQL/1_EasyProblems/1587_BankAccountSum.sql
@@ -6,3 +6,14 @@ FROM
 NATURAL JOIN Transactions
 GROUP BY name
 HAVING balance > 10000
+
+# OPTIMAL SOLUTION (LEETCODE)
+# Write your MySQL query statement below
+SELECT 
+    name        AS  "NAME",
+    SUM(amount) AS  "BALANCE"
+FROM 
+    users u INNER JOIN transactions t 
+    ON u.account = t.account
+GROUP BY name
+HAVING balance > 10000
