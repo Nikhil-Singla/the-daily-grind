33aa4e8387f067950dcf53fa3b61e775cec5c2bc
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-08 23:32:03 -0500

    Code commenting

--- Cpp_and_Python/1_easyProblems/1446_ConsecChar.cpp
+++ Cpp_and_Python/1_easyProblems/1446_ConsecChar.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     int maxPower(string s) 
     {
+        // Initialize variables to keep track of the current and maximum sequence length
         unsigned short c = 1, a = 1;
+        
+        // Loop through the string, starting from the second character
         for(unsigned short i = 1; i < s.length(); i++)
         {
+            // If the current character is the same as the previous character, increase the sequence length
             if(s[i] == s[i-1])
             {
                 c++;
+                
+                // Update the maximum sequence length if necessary
                 if(c > a) {a = c;}
-            } else c = 1;
+            } 
+            // If the current character is different from the previous character, reset the sequence length to 1
+            else c = 1;
         }    
+        // Return the maximum sequence length
         return a;
     }
 };
