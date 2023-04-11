cf115acc149c9199751912385a5f087944707b10
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-10 23:08:46 -0500

    Simple comments

--- Cpp_and_Python/1_easyProblems/2235_SumOfInt.cpp
+++ Cpp_and_Python/1_easyProblems/2235_SumOfInt.cpp
@@ -1,6 +1,9 @@
+// Define a class named "Solution"
 class Solution {
 public:
+    // Define a function named "sum" that takes two integer arguments "num1" and "num2"
     int sum(int num1, int num2) {
+        // Return the sum of "num1" and "num2"
         return(num1+num2);
     }
 };
