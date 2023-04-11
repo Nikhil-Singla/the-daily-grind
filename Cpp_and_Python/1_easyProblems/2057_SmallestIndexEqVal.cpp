33ca0c027ac75ebce94379a3f80c9db5a69f2e15
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-10 23:10:29 -0500

    Code commenting

--- Cpp_and_Python/1_easyProblems/2057_SmallestIndexEqVal.cpp
+++ Cpp_and_Python/1_easyProblems/2057_SmallestIndexEqVal.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int smallestEqual(vector<int>& nums) 
     {
+        // Loop through each number in the vector
         for(int i = 0, count = 0; i < nums.size(); i++, count++)
         {
+            // If count reaches 10, reset it to 0
             if(count == 10)
             {
                 count = 0;
             }
+            // If the current number is equal to count, return its index
             if(nums[i] == count)
             {
                 return i;
             }
         }
+        // If no number is equal to its index, return -1
         return -1;
     }
 };
