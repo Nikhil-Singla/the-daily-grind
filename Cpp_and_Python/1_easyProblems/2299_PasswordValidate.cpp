be9f0c30238ee0ec6d6e0c69271a5e6b13564d17
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-10 23:13:10 -0500

    Commenting first version

--- Cpp_and_Python/1_easyProblems/2299_PasswordValidate.cpp
+++ Cpp_and_Python/1_easyProblems/2299_PasswordValidate.cpp
@@ -1,26 +1,35 @@
+// This is the original code for solving LeetCode Problem 2299, 
+// checking if a given password is strong or not
 class Solution {
 public:
     bool strongPasswordCheckerII(string p) 
     {
+        // Define a string of special characters
         string special = "!@#$%^&*()-+";
+        // Define an array to check if password contains lower case letters, upper case letters, digits and special characters
         bool chk[5] = {false};
+        // If the password length is less than 8 characters, return false
         if(p.length() < 8)
         {return false;}
+        // Loop through each character in the password and check if it is a lower case letter
         for(int i = 0; i < p.length(); i++)
         {
             if(p[i] >= 'a' && p[i] <= 'z')
             {chk[0] = true;break;}
         }
+        // Loop through each character in the password and check if it is an upper case letter
         for(int i = 0; i < p.length(); i++)
         {
             if(p[i] >= 'A' && p[i] <= 'Z')
             {chk[1] = true;break;}
         }
+        // Loop through each character in the password and check if it is a digit
         for(int i = 0; i < p.length(); i++)
         {
             if(p[i] >= '0' && p[i] <= '9')
             {chk[2] = true;break;}
         }
+        // Loop through each character in the password and check if it is a special character
         for(int i = 0; i < p.length() && !chk[3]; i++)
         {
             for(auto &c: special)
@@ -29,15 +38,18 @@ public:
                 {chk[3] = true;break;}
             }
         }
+        // Check if any adjacent characters in the password are the same
         chk[4] = true;
         for(int i=1; i<p.size(); i++)
         {
             if(p[i-1]==p[i])
             {chk[4] = false;break;}
         }
+        // Return true if password contains at least one lower case letter, one upper case letter, one digit, one special character, and no adjacent characters are the same
         return(chk[0] && chk[1] && chk[2] && chk[3] && chk[4]);
     }
 };
+
 
 class Solution {
 public:
