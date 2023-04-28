b35a198cdcbd8fc61c637dba5ab51b05e3a0ec53
Nikhil-Singla <nsingla3.14@gmail.com>
2023-04-28 12:05:23 -0500

    Code commenting

--- Cpp_and_Python/1_easyProblems/2011_Operations.cpp
+++ Cpp_and_Python/1_easyProblems/2011_Operations.cpp
@@ -1,19 +1,27 @@
+// Define a class Solution
 class Solution {
 public:
+    // Define a function finalValueAfterOperations that takes a vector of strings called operations as input and returns an integer
     int finalValueAfterOperations(vector<string>& operations) 
     {
+        // Initialize an integer x to 0
         int x = 0;
+        // Iterate through each string in operations using a range-based for loop
         for(auto c : operations)
         {
+            // Check if the first or second character of the string is a plus sign
             if(c[0] == '+' || c[1] == '+')
             {
+                // If so, increment x by 1
                 x++;
             }
             else
             {
+                // Otherwise, decrement x by 1
                 x--;
             }
         }    
+        // Return the final value of x
         return x;
     }
 };
