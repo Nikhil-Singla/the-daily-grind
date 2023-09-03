77a91d2351cce71096e009fb0cac59a24bcdd996
Nikhil-Singla <nsingla3.14@gmail.com>
2023-09-02 23:58:46 -0500

    Code complete

--- Leetcode/Cpp_and_Python/2_mediumProblems/0062_UniquePaths.cpp
+++ Leetcode/Cpp_and_Python/2_mediumProblems/0062_UniquePaths.cpp
@@ -1,3 +1,36 @@
+// Solution that solves the problem using mathematics and logic
+
+class Solution {
+public:
+
+    int uniquePaths(int m, int n) 
+    {
+        int highest = max(m,n);
+        if(m == 1 || n == 1)
+        {
+            return 1;
+        } 
+        
+        int maze[m][n];
+        for(int i = 0; i < m; i++)
+        {
+            maze[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++)
+        {
+            maze[0][i] = 1;
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                maze[i][j] = maze[i-1][j] + maze[i][j-1];
+            }
+        }
+        return maze[m-1][n-1];
+    }
+
 
 
 // Initial Solution that exceeds time limit
@@ -11,3 +44,4 @@ public:
         return (uniquePaths(m, n, i+1, j) + uniquePaths(m, n, i, j+1));     
     }
 };
+
