7e1afddb3f28e013067fb40c861affb0ee9cafb6
Nikhil-Singla <nsingla3.14@gmail.com>
2023-09-07 23:27:56 -0500

    Daily Leetcode Streak 2

--- Leetcode/Cpp_and_Python/1_easyProblems/0118_Pascals.cpp
+++ Leetcode/Cpp_and_Python/1_easyProblems/0118_Pascals.cpp
@@ -2,6 +2,28 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) 
     {
+        vector<vector<int>> triangle;
+        vector<int> row = {1};
+        triangle.push_back(row);
+        if(numRows == 1)
+        {return triangle;}
+        for(int i = 1; i < numRows; i++)
+        {
+            vector<int> temp = {1, 1};
+            for(int j = 1; j < i; j++)
+            {
+                temp.insert(temp.begin()+1, triangle[i-1][j-1] + triangle[i-1][j]);
+            }
+            triangle.push_back(temp);
+        }
+        return triangle;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
         vector<vector<int>> v;
         v.push_back({1});
         for(int i = 1; i < numRows; i++)
