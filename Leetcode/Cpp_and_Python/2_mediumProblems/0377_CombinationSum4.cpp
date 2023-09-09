f209ef020073daaa85b1aaa8e805555f9a7f5fab
Nikhil-Singla <nsingla3.14@gmail.com>
2023-09-08 23:02:27 -0500

    Code successfully completed

---
+++ Leetcode/Cpp_and_Python/2_mediumProblems/0377_CombinationSum4.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) 
+    {
+        int ans = 0;
+        vector<int> dp(target+1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= target; i++)
+        {
+            for(auto &c : nums)
+            {
+                if(i - c >= 0)
+                {
+                    dp[i] += dp[i - c];
+                }
+            }  
+        }
+        return dp[target];
+    }
+};
