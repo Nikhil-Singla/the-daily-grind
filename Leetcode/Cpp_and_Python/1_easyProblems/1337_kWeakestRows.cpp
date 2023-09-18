3f4a8fb6b74b399c73a99e6a0f5133f45d1c8832
Nikhil-Singla <nsingla3.14@gmail.com>
2023-09-17 23:45:19 -0500

    Daily Leetcode Streak

---
+++ Leetcode/Cpp_and_Python/1_easyProblems/1337_kWeakestRows.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    static bool cmp(const pair<int, int>& a, const pair<int, int>& b)
+    {
+        if (a.second == b.second)
+        {
+            return a.first < b.first;
+        }
+        return a.second < b.second;
+    }
+
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k)
+    {
+        map<int, int> hash;
+        vector<int> ans;
+        for (uint i = 0; i < mat.size(); i++)
+        {
+            uint strength = 0;
+            for (auto& d : mat[i])
+            {
+                if (d)
+                {
+                    strength++;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            hash[i] = strength;
+        }
+        vector<pair<int, int>> A;
+
+        for (const auto& it : hash)
+        {
+            A.push_back(it);
+        }
+
+        // Sort using comparator function
+        sort(A.begin(), A.end(), cmp);
+
+        // Print the sorted value
+        for (int i = 0; i < k; i++)
+        {
+            ans.push_back(A[i].first);
+        }
+        return ans;
+    }
+};
