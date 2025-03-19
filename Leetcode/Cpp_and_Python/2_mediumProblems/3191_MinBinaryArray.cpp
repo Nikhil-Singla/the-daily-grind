4672850d5731dde8266b3af6afa3a0eeb2c572bd
Nikhil-Singla <nsingla3.14@gmail.com>
2025-03-18 23:41:18 -0700

    Bit manip and code optimization for 100% score

--- Leetcode/Cpp_and_Python/2_mediumProblems/3191_MinBinaryArray.cpp
+++ Leetcode/Cpp_and_Python/2_mediumProblems/3191_MinBinaryArray.cpp
@@ -3,15 +3,13 @@ public:
     int minOperations(vector<int>& nums) 
     {
         uint count = 0, i;
-        uint length = nums.size()-2;
         for(i = 0; i < nums.size()-2; i++)
         {
-            if (nums[i] == 0)
+            if (!nums[i])
             {
                 count++;
-                nums[i] = 1;
-                nums[i+1] = (nums[i+1] ? 0 : 1);
-                nums[i+2] = (nums[i+2] ? 0 : 1);
+                nums[i+1] ^= 1;
+                nums[i+2] ^= 1;
             
             }
         }
