279c80078ff6b6e189f1be25f61a27e59e5d121c
Nikhil-Singla <nsingla3.14@gmail.com>
2025-03-25 23:31:40 -0700

    Code optimization by removing redundant median calculation

--- Leetcode/Cpp_and_Python/2_mediumProblems/2033_MinOpUniGrid.py
+++ Leetcode/Cpp_and_Python/2_mediumProblems/2033_MinOpUniGrid.py
@@ -2,8 +2,9 @@ import math as m
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         arrayElement = []
-        median = [0, 0]
+        median = 0
         ongoingRemainder = grid[0][0] % x
+        
         for oneRow in grid:
             for oneElement in oneRow:
                 if oneElement%x != ongoingRemainder:
@@ -13,23 +14,15 @@ class Solution:
 
         arrayElement.sort()
         itemCount = len(arrayElement)
+
         if itemCount == 1:
             return 0
 
-        if itemCount%2:
-            itemCount -= 1
-            median[0] = arrayElement[int(itemCount/2)]
-            median[1] = arrayElement[int(itemCount/2)]
-        else:
-            median[0] = arrayElement[int(itemCount/2)] 
-            median[1] = arrayElement[int(itemCount/2)-1]
-        
-        steps = [0, 0]
+        median = arrayElement[int(itemCount/2)] 
+        steps = 0
 
         for eachElement in arrayElement:
-            if eachElement != median[0]: 
-                steps[0] += abs(eachElement - median[0])/x
-            if eachElement != median[1]: 
-                steps[1] += abs(eachElement - median[1])/x
+            if eachElement != median: 
+                steps += abs(eachElement - median)/x
 
-        return int(min(steps))
+        return int(steps)
