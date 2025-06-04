e6dd2b7a2891c5f8b38493e92039aad4b152709f
Nikhil-Singla <nsingla3.14@gmail.com>
2025-06-03 18:56:08 -0700

    Fixed lexicograph misunderstanding, retried

--- Leetcode/Cpp_and_Python/2_mediumProblems/3403_FindLexHighest.py
+++ Leetcode/Cpp_and_Python/2_mediumProblems/3403_FindLexHighest.py
@@ -8,30 +8,49 @@ class Solution:
             for letter in word:
                 answer = max(answer, ord(letter))
             return chr(answer)
-
-        answer = ['a'] * (excess+1)
-
-        for index, count in enumerate(word):
-            if (index + excess) >= size:
-                break
-
-            current = word[index : index+excess+1]
-            # print(current)
-
-            for idx, letter in enumerate(answer):
-                comparator = ord(answer[idx]) - ord(current[idx])
-                
-                if comparator < 0:
-                    for i in range(idx, excess - idx + 1):
-                        answer[i] = current[i]
-                elif comparator > 0:
-                    break
-                else:
-                    continue
         
+        highestList = []
+        # print(ord('d'), ord('a'))
+
+        for index, letter in enumerate(word):
+            if answer == ord(letter):
+                highestList.append(index)
+            elif answer < ord(letter):
+                highestList = []
+                highestList.append(index)
+                answer = ord(letter)
+            elif answer > ord(letter):            
+                continue
+
+        word = list(word)
+        startSpot = highestList[0]
+        answer = word[startSpot:startSpot+excess+1]
+        # print(word, answer, highestList)
+
+        for spot in highestList:
+            current = word[spot:spot+excess+1]
+            curSize = len(current)
+            anSize = len(answer)
+            
+            if curSize == anSize:
+                for i in range(curSize):
+                    comparator = ord(answer[i]) - ord(current[i])
+
+                    if comparator < 0:
+                        for idx in range(i, len(answer)):
+                            # print(answer)
+                            answer[idx] = current[idx]
+                    elif comparator > 0:
+                        break
+                    else:
+                        continue
+
+            if curSize > anSize:
+                answer = current
+            
         retVal = ""
         for s in answer:
             retVal += s 
 
         return retVal
-        
+
