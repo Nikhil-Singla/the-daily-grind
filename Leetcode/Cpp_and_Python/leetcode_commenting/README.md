7421c96096c64f9278f8b77f0d223e76bc20bc34
Nikhil-Singla <nsingla3.14@gmail.com>
2025-07-04 07:23:40 -0700

    Code passing all testcases. Accepted Successfully.

--- Leetcode/Cpp_and_Python/leetcode_commenting/README.md
+++
@@ -1,2 +0,0 @@
-# leetcode_commenting
-Learning medium/hard puzzles in Leetcode by commenting the solutions that I got from the website.
--- leetcode/cpp/01_easy/0001_TwoSum.cpp
+++ leetcode/cpp/01_easy/0001_TwoSum.cpp
@@ -2,6 +2,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i<nums.size()-1; i++) //First run through the entire array with the i'th element starting from 0 
+        {
+            for(int j = i+1; j<nums.size();j++) //At the same time, run with the j'th element which is starting from i+1
+            {
+                if((nums[i]+nums[j])==target) //If the sum of i'th and the j'th element is target, then output
+                {
+                    return{i, j}; //Return as vector
+                }
+            }
+        }
+        return {}; //Return null if no solution
+    }
+};
+
+
+#include <stdlib.h>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
         vector<pair<int, int>> pairs; //Declare vector of datatype "pair" and both elements of the pair/tuple being int
         for(int i=0; i<nums.size();i++)
         {
