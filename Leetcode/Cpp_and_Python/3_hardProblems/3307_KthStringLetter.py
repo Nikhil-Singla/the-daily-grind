c057582d931f3a4449e5c9846647dfb8119905e4
Nikhil-Singla <nsingla3.14@gmail.com>
2025-07-04 02:26:46 -0700

    Needs some logic correction, but mostly finished.

---
+++ Leetcode/Cpp_and_Python/3_hardProblems/3307_KthStringLetter.py
@@ -0,0 +1,33 @@
+class Solution:
+    def kthCharacter(self, k: int, operations: List[int]) -> str:
+
+        start = 'a'
+        count = 1
+
+        reverse = []
+        shift = 0
+
+        for idx, i in enumerate(operations):
+            # print(idx, i, count)
+            
+            if count >= k:
+                reverse = operations[idx::-1]
+                # print(reverse)
+                break
+
+            count *= 2
+
+        if not reverse:
+            reverse = operations[::-1]
+
+        for i in reverse:
+            print(count, k)
+            count //= 2
+            
+            if i:
+                if k > count:
+                    shift += 1
+            
+            k -= (count // 2)    
+
+        return chr((shift % 26) + ord('a'))
