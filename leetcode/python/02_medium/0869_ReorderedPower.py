9af51e122b8da6face23a0412019bfa45151ae36
Nikhil Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-09 21:28:09 -0700

    Code passing all testcases, beats 60%

--- leetcode/python/02_medium/0869_ReorderedPower.py
+++ leetcode/python/02_medium/0869_ReorderedPower.py
@@ -3,7 +3,12 @@ class Solution:
         MAX_VALUE = 10**9
         
         def get_digits(number):
-            return ''.join(sorted(str(number)))
+            digits = []
+            while number > 0:
+                digits.append(number%10)
+                number //= 10
+
+            return ''.join([str(s) for s in sorted(digits)])
 
         power_set = set()
         
