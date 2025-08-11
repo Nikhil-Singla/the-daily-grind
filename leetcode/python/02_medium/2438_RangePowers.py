a5bb540cd8f9e3cf0fa35099367fd41b2d7c0437
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-10 20:44:39 -0700

    Getting base cases setup and lists ready. Final answer remaining

---
+++ leetcode/python/02_medium/2438_RangePowers.py
@@ -0,0 +1,35 @@
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        MOD = 10**9 + 7
+        
+        powers = []
+        
+        for i in range(32):
+            mask = 1 << i
+            if mask & n:
+                powers.append(pow(2, i))
+
+        length = len(powers)
+        prefMult = [powers[0]]
+        sufMult = [powers[-1]]
+        maxMult = powers[0]
+
+        for i in range(1, length):
+            prefMult.append((powers[i] * prefMult[i-1] ) % MOD)
+            sufMult.append((powers[length-i-1] * sufMult[i-1]) % MOD)
+            maxMult = (maxMult * powers[i]) % MOD
+
+
+        sufMult.reverse()
+        # print(powers, sufMult, prefMult)
+        answers = []
+
+        for start, end in queries:
+            if start == 0:
+                answers.append(prefMult[end])
+            elif end == length - 1:
+                answers.append(sufMult[start])
+            
+
+
+        return 0
