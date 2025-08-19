5101612311dedf0c7a37ae36ebb11bb31b3cfa30
Nikhil Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-18 21:34:43 -0700

    Code passing all testcases, beats 100%. Added caching method, speed improved by 20%

--- leetcode/python/02_medium/2348_ZeroFilledSub.py
+++ leetcode/python/02_medium/2348_ZeroFilledSub.py
@@ -4,13 +4,33 @@ class Solution:
         counter = 0
 
         for i in nums:
-            if i != 0:
+            if i == 0:
+                counter += 1
+            elif counter:
                 ans += (counter * (counter+1))//2
                 counter = 0
-            else:
-                counter += 1
 
         if counter:
             ans += (counter * (counter+1))//2
             
+        return ans
+
+# CACHING Method, but slightly slower due to smaller value of nums.length():
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        ans = 0
+        counter = 0
+        hot_cache = {}
+
+
+        for i in nums:
+            if i == 0:
+                counter += 1
+            elif counter:
+                ans += hot_cache.setdefault(counter, (counter * (counter+1)) // 2)
+                counter = 0
+        
+        if counter:
+            ans += hot_cache.setdefault(counter, (counter * (counter+1)) // 2)
+
         return ans
