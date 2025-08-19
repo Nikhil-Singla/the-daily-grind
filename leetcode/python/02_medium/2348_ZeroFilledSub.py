fbf7c0b69e1ba411f8a569c5e0d8df3b1d6c5a64
Nikhil Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-18 21:29:13 -0700

    Code passing all testcases, beats 100%

--- leetcode/python/02_medium/2348_ZeroFilledSub.py
+++ leetcode/python/02_medium/2348_ZeroFilledSub.py
@@ -1,17 +1,16 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        def sum_to_n(number):
-            return (number * (number+1))//2
-
         ans = 0
         counter = 0
 
         for i in nums:
-            if i == 0:
-                counter += 1
-            elif counter != 0:
-                ans += sum_to_n(counter)
+            if i != 0:
+                ans += (counter * (counter+1))//2
                 counter = 0
+            else:
+                counter += 1
 
-        ans += sum_to_n(counter)
+        if counter:
+            ans += (counter * (counter+1))//2
+            
         return ans
