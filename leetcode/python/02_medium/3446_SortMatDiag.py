2cc6db093290be38e4c10f620dbf74b205f04024
Nikhil Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-27 20:48:25 -0700

    [SPEED OPTIMAL - 99] [MEMORY EFFICIENT - 85]
    Optimized for speed, finished in 3ms and beats 98.75% of the code. Efficient usage of memory, 17.7MB lets it beat 82.92% of the code

--- leetcode/python/02_medium/3446_SortMatDiag.py
+++ leetcode/python/02_medium/3446_SortMatDiag.py
@@ -1,23 +1,25 @@
 class Solution:
     def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
-
+        
+        # Iterates from 0, 0 -> n, 0 for the inner loop
         for i in range(n):
             array_size = n - i
             arr = []
 
             for j in range(array_size):
-                heappush(arr, -grid[i+j][j])
+                heappush(arr, -grid[i+j][j])    # Negative to implement max heap, and sort by descending order as data is added
 
             for j in range(array_size):
                 grid[i+j][j] = -heappop(arr)
-
+        
+        # Iterates from 0, 1 -> 0, n for the outer loop
         for i in range(1, n):
             array_size = n - i
             arr = []
 
             for j in range(array_size):
-                heappush(arr, grid[j][i+j])
+                heappush(arr, grid[j][i+j])     # Positive to implement min heap, and sort by ascending order as data is added
 
             for j in range(array_size):
                 grid[j][i+j] = heappop(arr)
