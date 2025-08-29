45e52c03d250bb171453083050936d8e027063fc
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-08-28 22:42:18 -0700

    [A3] [IMP] [SO] [MO]

--- leetcode/python/02_medium/3021_FlowerGame.py
+++ leetcode/python/02_medium/3021_FlowerGame.py
@@ -1,13 +1,3 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        n
-        even = n // 2
-        odd = (n+1) // 2
-        
-        m
-        even = m // 2
-        odd = (m+1) // 2
-
-        ans = ((n // 2) * ((m+1) // 2))) + (((n+1) // 2) * (m // 2))
-
-        return ans
+        return ((n // 2) * ((m+1) // 2)) + (((n+1) // 2) * (m // 2))
