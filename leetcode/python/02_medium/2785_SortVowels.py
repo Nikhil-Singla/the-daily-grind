05d2c5911e7f37d3c75bfe00238b8911b1cdc9d1
Nikhil Singla <nsingla3.14@gmail.com>
2025-09-10 18:09:06 -0700

    [A2] [S.CHECK] [MO] [ALT] [CPY] [IGN]

--- leetcode/python/02_medium/2785_SortVowels.py
+++ leetcode/python/02_medium/2785_SortVowels.py
@@ -1,3 +1,4 @@
+[A1] [SE] [ME] 
 class Solution:
     def sortVowels(self, s: str) -> str:
         vowels = set(['a','e','i','o','u','A','E','I','O','U'])
@@ -18,3 +19,32 @@ class Solution:
                 j += 1
         
         return ''.join(s)
+
+[A2] [S.CHECK] [MO] [ALT] [CPY] [IGN]
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        vowels = {'A':0, 'E':1, 'I':2, 'O':3, 'U':4, 'a':5, 'e':6, 'i':7, 'o':8, 'u':9}
+        vowels_rvrs = {0:'A', 1:'E', 2:'I', 3:'O', 4:'U', 5:'a', 6:'e', 7:'i', 8:'o', 9:'u'}
+        vowels_cnt = [0]*len(vowels)
+        for char in s:
+            if self.is_vowel(char):
+                vowels_cnt[vowels[char]] += 1
+        
+        index = 0
+        while index < len(vowels) and vowels_cnt[index] == 0:
+            index += 1
+
+        for i, char in enumerate(s):
+            if self.is_vowel(char):
+                s = s[:i] + vowels_rvrs[index] + s[i+1:]
+                vowels_cnt[index] -= 1
+                while index < len(vowels) and vowels_cnt[index] == 0:
+                    index += 1
+        return s
+
+    def is_vowel(self, c: chr) -> bool:
+        c = c.lower()
+        if c == 'a' or c == 'e' or c == 'i' or c == 'o' or c == 'u':
+            return True
+        else:
+            return False
