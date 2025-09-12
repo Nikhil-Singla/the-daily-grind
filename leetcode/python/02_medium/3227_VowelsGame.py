c97f94c27748467a3a7e9d4524be8fcec19813e6
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-09-11 21:09:04 -0700

    [A2] [SO] [MO] Using C implemented regex and stopping if one instance is found

--- leetcode/python/02_medium/3227_VowelsGame.py
+++ leetcode/python/02_medium/3227_VowelsGame.py
@@ -1,3 +1,5 @@
+# [A1] [ME] [SE]
+
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         vowels = set(['a', 'e', 'i', 'o' , 'u'])
@@ -7,3 +9,10 @@ class Solution:
                 return True
 
         return False
+    
+
+# [A2] [SO] [MO]
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        return bool(re.search(r"[aeiou]", s))
+
