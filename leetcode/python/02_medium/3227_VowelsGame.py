461473716cf2aa670f2de5a9228e6de6204079f0
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-09-11 21:10:42 -0700

    [A3] [SO] [MO] [ALT] C implemented solution without regex for bounded strings

--- leetcode/python/02_medium/3227_VowelsGame.py
+++ leetcode/python/02_medium/3227_VowelsGame.py
@@ -1,5 +1,4 @@
 # [A1] [ME] [SE]
-
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         vowels = set(['a', 'e', 'i', 'o' , 'u'])
@@ -12,7 +11,14 @@ class Solution:
     
 
 # [A2] [SO] [MO]
+
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
         return bool(re.search(r"[aeiou]", s))
+
+# [A3] [SO] [ME] [ALT]
+class Solution:
+    def doesAliceWin(self, s: str) -> bool:
+        if s.count("a") + s.count("e") + s.count("i") + s.count("o") + s.count("u"): return True
+        else: return False
 
