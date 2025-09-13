9c69badc89eb65dbc92af56840b414b3af260686
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-09-12 19:34:21 -0700

    [A1] [MO] [SE] Basic solution

---
+++ leetcode/python/01_easy/3451_FindMaxVowelsCons.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxFreqSum(self, s: str) -> int:
+        s = Counter(s)
+        vowels = set(['a', 'e', 'i', 'o', 'u'])        
+        maxV = 0
+        maxC = 0
+
+        a = [0]
+        b = [0]
+
+        for i in s.keys():
+            if i in vowels:
+                a.append(s[i])
+            else:
+                b.append(s[i])
+
+        return max(a) + max(b)
