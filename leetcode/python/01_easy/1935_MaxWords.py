72d84dc42cb9405e86ac63b18b325f5b614993b4
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-09-14 17:42:45 -0700

    [A2] [IMP] [SO] [MO]

--- leetcode/python/01_easy/1935_MaxWords.py
+++ leetcode/python/01_easy/1935_MaxWords.py
@@ -1,5 +1,4 @@
 # [A1] [SO] [M.CHECK]
-
 class Solution:
     def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
         text = text.split(' ')
@@ -8,6 +7,20 @@ class Solution:
 
         for word in text:
             ans += 1
+            for letter in word:
+                if letter in brokenLetters:
+                    ans -= 1
+                    break
+
+        return ans
+
+# [A2] [IMP] [SO] [MO]
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        text = text.split(' ')
+        ans = len(text)
+
+        for word in text:
             for letter in word:
                 if letter in brokenLetters:
                     ans -= 1
