91a7294868847c8206f557c4b38bf55d3f7f67e2
Nikhil-Singla <55959861+Nikhil-Singla@users.noreply.github.com>
2025-09-27 07:01:24 -0700

    [A2] [IMP] [SE] [M.CHECK]

--- leetcode/python/01_easy/0812_LargestTriangle.py
+++ leetcode/python/01_easy/0812_LargestTriangle.py
@@ -1,19 +1,13 @@
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
-        def shoelaceFormula(point1, point2, point3):
-            xa, ya = map(float, point1)
-            xb, yb = map(float, point2)
-            xc, yc = map(float, point3)
-            
-            return abs(((xa-xc)*(yb-ya))-((xa-xb)*(yc-ya)))
-
         ans = 0
         n = len(points)
         for i in range(n):
+            xa, ya = points[i]
             for j in range(i+1, n):
+                xb, yb = points[j]
                 for k in range(j+1, n):
-
-                    area = shoelaceFormula(points[i], points[j], points[k])
-                    ans = max(ans, area)
+                    xc, yc = points[k]
+                    ans = max(ans, abs(((xa-xc)*(yb-ya))-((xa-xb)*(yc-ya))))
 
         return ans*0.5
