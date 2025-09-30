ef9bcb1bf71a440234f271fe87382ca03ff19cb8
Nikhil Singla <nsingla3.14@gmail.com>
2025-09-30 12:23:14 -0500

    [A1] [SE] [M.CHECK] Brute Force attempt

---
+++ leetcode/2221_FindTriangularSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        while (len(nums) > 1):
+            temp = []
+            for i in range(len(nums[:-1])):
+                temp.append(nums[i] + nums[i+1])
+
+            nums = temp
+
+        return (nums[0] % 10)
