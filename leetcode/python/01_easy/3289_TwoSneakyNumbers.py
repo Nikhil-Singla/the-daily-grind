5f177a60d9979dc70332781c5dcad1ab897d34ef
Nikhil Singla <nsingla3.14@gmail.com>
2025-10-31 03:26:54 -0700

    [A2] [SO] [MF] Faster solution but consumes more memory

--- leetcode/python/01_easy/3289_TwoSneakyNumbers.py
+++ leetcode/python/01_easy/3289_TwoSneakyNumbers.py
@@ -1,11 +1,24 @@
 class Solution:
     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
-        bits = 0
+        seen = [0] * len(nums)
         ans = []
         for i in nums:
-            if bits & (1 << i) != 0: 
+            if seen[i]:
                 ans.append(i)
             
-            bits |= 1 << i
+            seen[i] = 1
 
         return ans
+
+# Bits solution
+# class Solution:
+#     def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+#         bits = 0
+#         ans = []
+#         for i in nums:
+#             if bits & (1 << i) != 0: 
+#                 ans.append(i)
+            
+#             bits |= 1 << i
+
+#         return ans
