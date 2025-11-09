1ea399956694d986e21ffb280fc705e359f7f48d
Nikhil Singla <nsingla3.14@gmail.com>
2025-11-08 16:08:46 -0800

    [A2] [SO] [MO] Optimized code to maximize speed and memory requirements

--- leetcode/python/01_easy/2169_CountOp.py
+++ leetcode/python/01_easy/2169_CountOp.py
@@ -2,10 +2,13 @@ class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
         count = 0
         while num1 and num2:
-            count += 1
             if num1 > num2:
-                num1 -= num2
+                temp = num1 // num2
+                count += temp
+                num1 -= num2*temp
             else:
-                num2 -= num1
+                temp = num2 // num1
+                count += temp
+                num2 -= num1*temp
 
         return count
