1fc0dc6731bda7acadfe63dd3e1a9bc2d06b9f4a
Nikhil Singla <nsingla3.14@gmail.com>
2026-03-04 09:36:37 -0800

    [A2] [SO] 2-Pass, Improved solution's speed complexity for larger test cases

--- leetcode/python/01_easy/1582_SpecBin.py
+++ leetcode/python/01_easy/1582_SpecBin.py
@@ -3,19 +3,19 @@ class Solution:
         m = len(mat)
         n = len(mat[0])
         count = 0
+
+        row_range = [0] * m
+        col_range = [0] * n
+
         for i in range(m):
             for j in range(n):
                 if mat[i][j] == 1:
-                    valid = True
-                    for l in range(m):
-                        if mat[l][j] == 1 and l != i:
-                            valid = False
-                            break
-                    if valid:    
-                        for k in range(n):
-                            if mat[i][k] == 1 and k != j:
-                                valid = False
-                                break
-                    if valid:
-                        count += 1
+                    row_range[i] += 1
+                    col_range[j] += 1            
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and row_range[i] == 1 and col_range[j] == 1:
+                    count += 1
+
         return count
