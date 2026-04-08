ac1dffb27ddf6171588855e6e3bfc1a13ce957f1
Nikhil Singla <nsingla3.14@gmail.com>
2026-04-07 23:19:37 -0700

    [A1] [SO] [ME]

---
+++ leetcode/python/02_medium/3653_XORMultiplication.py
@@ -0,0 +1,12 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        mod = 10**9 + 7
+        for l, r, k ,v in queries:
+            for i in range(l, r+1, k):
+                nums[i] = (nums[i]*v) % mod
+
+        ret = nums[0]
+        for i in nums[1:]:
+            ret ^= i
+
+        return ret
