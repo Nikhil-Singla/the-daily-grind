c379a8f93e70a46c6b569a5a7987d69a93646e42
Nikhil Singla <nsingla3.14@gmail.com>
2026-07-31 03:37:41 -0700

    [A1] [SO] [MF] Using simple dict mapping. Can be improved by using count instead

---
+++ leetcode/python/02_medium/3016_MinPushes2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        alpha = Counter(word)
+        f_map = 8
+        s_map = 8
+        t_map = 8
+        fo_map = 2
+
+        pushes = 0
+
+        for i, _ in sorted(alpha.items(), key=lambda item: item[1], reverse=True):
+            count = alpha[i]
+            if count <= 0:
+                continue
+
+            if f_map:
+                f_map -= 1
+                pushes += count
+            elif s_map:
+                s_map -= 1
+                pushes += count*2
+            elif t_map:
+                t_map -= 1
+                pushes += count*3
+            else:
+                fo_map -= 1
+                pushes += count*4
+
+        return pushes
