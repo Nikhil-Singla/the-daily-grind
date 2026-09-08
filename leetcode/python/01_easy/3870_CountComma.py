b0f3823fdceb3599a27a2cbdf0b11eb26f0a31a5
Nikhil Singla <nsingla3.14@gmail.com>
2026-09-07 17:37:51 -0700

    [A2] [SO] [MO] Simplified solution

--- leetcode/python/01_easy/3870_CountComma.py
+++ leetcode/python/01_easy/3870_CountComma.py
@@ -2,9 +2,5 @@ class Solution:
     def countCommas(self, n: int) -> int:
         if n < 1000:
             return 0
-        
-        n = n - 1000    # Since n can only be upto 100,000
-                        # This means after 999, every number can ONLY 
-                        # have one comma at most. 
-
-        return n + 1    # Add one as we start counting from "0" 
+        else:
+            return n - 999
